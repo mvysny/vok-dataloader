189e528c6680f9d8d7e816e390bc53c1146110d7
Martin Vysny <martin@vysny.me>
2019-02-14 15:24:26 +0200

    v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ if (localProperties.exists()) {
 }
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.3.10"
+    id("org.jetbrains.kotlin.jvm") version "1.3.21"
     id("com.jfrog.bintray") version "1.8.3"
     `maven-publish`
     id("org.jetbrains.dokka") version "0.9.17"
@@ -31,7 +31,7 @@ tasks.withType<KotlinCompile> {
 }
 
 dependencies {
-    compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    compile(kotlin("stdlib-jdk8"))
 
     // tests
     testCompile("com.github.mvysny.dynatest:dynatest-engine:0.13")
